4a2de94cd80737af0f58edf3504c7c04224d2e58
Xue Hong <dearshor@gmail.com>
2016-01-24 12:43:25 +0800

    clean up code
    Signed-off-by: Xue Hong <dearshor@gmail.com>

--- src/test/java/org/dearshor/fibonacci/FibonacciCalculatorTest.java
+++ src/test/java/org/dearshor/fibonacci/FibonacciCalculatorTest.java
@@ -22,7 +22,15 @@ public class FibonacciCalculatorTest {
 
     @BeforeClass
     public static void setUp() throws Exception {
-        cache = new ConcurrentHashMap<>(10000); calculator = new FibonacciCalculator(cache);
+        cache = new ConcurrentHashMap<>(100000);
+        calculator = new FibonacciCalculator(cache);
+        // warm-up
+        long begin = System.currentTimeMillis();
+
+        calculator.calculate(100000L);
+        long end = System.currentTimeMillis();
+        System.out.printf("warm-up: elapsed time: %d  ms\n", end - begin);
+
     }
 
     @After
@@ -30,7 +38,7 @@ public class FibonacciCalculatorTest {
 
     }
 
-    @Test
+//    @Test
     public void testCalculate2() throws Exception {
 
         System.out.printf("fibonacci(100) = %s \n", calculator.calculate2(100L));
@@ -42,7 +50,7 @@ public class FibonacciCalculatorTest {
         System.out.printf("fibonacci(1000000) = %s \n", calculator.calculate2(1000000L));
     }
 
-    @Test
+//    @Test
     public void testCalculate3() throws Exception {
 
         System.out.printf("fibonacci(100) = %s \n", calculator.calculate3(100L));
@@ -62,21 +70,34 @@ public class FibonacciCalculatorTest {
         }*/
     }
 
-    @Test
+//    @Test
     public void testCalculate() throws Exception {
-        System.out.printf("fibonacci(100) = %s \n", calculator.calculate(100L));
-        System.out.printf("fibonacci(1000) = %s \n", calculator.calculate(1000L));
-        System.out.printf("fibonacci(1500) = %s \n", calculator.calculate(1500L));
-        System.out.printf("fibonacci(10000) = %s \n", calculator.calculate(10000L));
-        System.out.printf("fibonacci(20000) = %s \n", calculator.calculate(20000L));
-        System.out.printf("fibonacci(100000) = %s \n", calculator.calculate(100000L));
-        System.out.printf("fibonacci(1000000) = %s \n", calculator.calculate(1000000L));
+        long begin = System.currentTimeMillis();
+//        System.out.printf("fibonacci(100) = %s \n", calculator.calculate(100L));
+//        System.out.printf("fibonacci(1000) = %s \n", calculator.calculate(1000L));
+//        System.out.printf("fibonacci(1500) = %s \n", calculator.calculate(1500L));
+//        System.out.printf("fibonacci(10000) = %s \n", calculator.calculate(10000L));
+//        System.out.printf("fibonacci(20000) = %s \n", calculator.calculate(20000L));
+//        System.out.printf("fibonacci(100000) = %s \n", calculator.calculate(100000L));
+        System.out.printf("fibonacci(10 0000) = %s \n", calculator.calculate(10 * 10000L));
 //        System.out.printf("fibonacci(Integer.MAX_VALUE) = %s \n", calculator.calculate3(Integer.MAX_VALUE));
+
+        long end = System.currentTimeMillis();
+        System.out.printf("testCalculate(10 0000): elapsed time: %d ms \n", end - begin);
     }
 
     @Test
     public void testCalculateAgain() throws Exception {
-        System.out.printf("fibonacci(1000000) = %s \n", calculator.calculate(1000000L));
+        long begin = System.currentTimeMillis();
+
+        System.out.println("##### test again #####");
+        System.out.printf("fibonacci(10 0000) = %s \n", calculator.calculate(10 * 10000L));
+        long end = System.currentTimeMillis();
+        System.out.printf("testCalculateAgain(10 0000): elapsed time: %d ms\n", end - begin);
+
+        System.out.printf("fibonacci(110 0000) = %s \n", calculator.calculate(110 * 10000L));
+        end = System.currentTimeMillis();
+        System.out.printf("testCalculateAgain(110 0000): elapsed time: %d ms\n", end - begin);
 
     }
 
